bba229c36cd4f6455be8b6e9d0ca6cc124f7f959
Md. Rishad Hossain <82581808+voidrishad11@users.noreply.github.com>
2023-11-29 23:58:03 +0600

    Initial commit

---
+++ hello.java
@@ -0,0 +1,11 @@
+class Homa
+{
+	public static void main (String[] args)
+	{
+		int a = 6, b = 90;
+		String fruit = "banana";
+		System.out.println(fruit);
+        System.out.println("vavavavavavavavav");
+        System.out.println(a+b);
+	}
+}
